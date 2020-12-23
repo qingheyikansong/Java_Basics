8818ea3061c11bc2eab29f14c17bf70aaad2c991
songyh <1264566874@qq.com>
2020-12-23 23:56:58 +0800

    HashMap学习day1

---
+++ Java_Basics/src/main/java/com/syh/java/basics/HashMapAnalyse.java
@@ -0,0 +1,11 @@
+package com.syh.java.basics;
+
+import java.util.HashMap;
+
+public class HashMapAnalyse {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put("","");
+        System.out.println(map.get(""));
+    }
+}
